bf3dc2fea52a7c62527f6f4c04e3aebb64add25f
Taj Foley <n11247070@qut.edu.au>
2024-08-27 09:13:02 +1000

    Initial SetUP #2

--- src/main/java/com/example/petcarecab302qu/HelloApplication.java
+++ src/main/java/com/example/petcarecab302qu/HelloApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 
 import java.io.IOException;
 
