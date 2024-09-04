603996286fb189b920f0946379897f10cc673d1c
Taj Foley <n11247070@qut.edu.au>
2024-09-04 21:21:47 +1000

    Login #4

--- src/main/java/com/example/petcarecab302qu/controller/LoginController.java
+++ src/main/java/com/example/petcarecab302qu/controller/LoginController.java
@@ -7,32 +7,41 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+
 public class LoginController {
     @FXML
-    private TextField Username;
+    private TextField Firstname;
     @FXML
-    private TextField Password;
+    private PasswordField Password;
     @FXML
     private Button Confirm;
     @FXML
     private Text error;
     @FXML
-    private SqliteContactDAO contactDAO = new SqliteContactDAO();
+    private Connection connection;
     @FXML
+    private SqliteContactDAO contactDAO = new SqliteContactDAO();
+
     protected void handlelogin() {
         //Gets username and password text
-        String username = Username.getText();
+        String firstname = Firstname.getText();
         String password = Password.getText();
 
-        //Checks if the username field is empty
-        if (username.isEmpty())
+        //Checks if the firstname field is empty
+        if (firstname.isEmpty())
         {
-            error.setText("Please provide username.");
+            error.setText("Please provide firstname.");
             error.setVisible(true);
             return;
         }
@@ -44,9 +53,24 @@ public class LoginController {
             return;
         }
 
-        //Checks that username and password are both contained in the database
-
-
+        //Checks that username and password are both contained in the database under the same ID
+        try {
+            String query = "SELECT * FROM contacts WHERE firstName = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1,firstname);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                // Valid firstname and password
+            } else {
+                // Invalid firstname and password
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Clears Password field
+        Firstname.clear();
+        Password.clear();
 
 
 
