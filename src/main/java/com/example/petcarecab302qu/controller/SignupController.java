89bf930544afec143d53f2565740c7eda6cfff0e
Anh <anhduongnguyen51015@gmail.com>
2024-09-05 11:18:06 +1000

    added back button in signup

--- src/main/java/com/example/petcarecab302qu/controller/SignupController.java
+++ src/main/java/com/example/petcarecab302qu/controller/SignupController.java
@@ -1,11 +1,18 @@
 package com.example.petcarecab302qu.controller;
 
+import com.example.petcarecab302qu.HelloApplication;
 import com.example.petcarecab302qu.model.Contact;
 import com.example.petcarecab302qu.model.SqliteContactDAO;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignupController {
 
@@ -62,4 +69,12 @@ public class SignupController {
         errorMessage.setStyle("-fx-text-fill: green;");
         errorMessage.setVisible(true);
     }
+    public void handleBackButton(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
+        Scene scene = new Scene(loader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
 }
+
+
